0f8a51421ad6110829b63246023b0402e1f59cd6
Luiz Almeida <luiz.almeida@NB-1374.local>
2020-10-13 10:42:58 -0300

    sanitize return

--- Models/Score.js
+++ Models/Score.js
@@ -12,6 +12,16 @@ const schema = new Schema({
     }
 });
 
+schema.set('toJSON', {
+    virtuals: true,
+    transform: (doc, ret, options) => {
+        delete ret.__v;
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.id;
+    },
+});
+
 let ScoreModel = mongoose.model('score', schema);
 
 module.exports = ScoreModel;
